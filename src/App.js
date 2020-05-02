65433e5d135293a23549b330d19a791cc8b24c3b
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-02 21:57:31 +0200

    Another page added for FiveDaysForecast

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Header from './components/Header.js/Header';
+import FiveDaysForecast from './pages/FiveDaysForecast/FiveDaysForecast';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
   <Header/>
     <Switch>
       <Route exact path='/' component={Home}/>
+      <Route path='/FiveDays' component={FiveDaysForecast}/>
     </Switch>
    </BrowserRouter>
 
---
+++ src/pages/FiveDaysForecast/FiveDaysForecast.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FiveDaysForecast extends Component {
+
+    render() {
+        return (
+            <div>
+                5 dniowa pogoda
+            </div>
+        )
+    }
+}
+
+export default FiveDaysForecast;
