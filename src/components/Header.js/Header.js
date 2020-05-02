94862b1395929248bdbf02102d94f9aa43d330ae
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-02 09:55:15 +0200

    useState created/  prepare for fetch from apiWether

--- src/components/Header.js/Header.js
+++ src/components/Header.js/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
     
     return(
-        <div>
-            probably the best header in this app
+        <div className='headerTitle'>
+            Probably the best header in this app!
         </div>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 function Home() {
 
+    const [currentWeather, setCurrentWeather] = useState({})
+
+        // downloadCurrentWeather = () => {
+        //     fetch()
+        // }
+   
         return(
             <div>
                 <h1>Probably the best weather App in the World</h1>
+                <h3>Na tej stronie będziemy wyświetlać główną stronę aplikacji.</h3>
             </div>
         )
 }
