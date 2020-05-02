7e8bb21c6cf8e47c14fda4ed49021b0812550ff6
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-02 12:09:22 +0200

    fetch from api work

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,44 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 
-function Home() {
 
-    const [currentWeather, setCurrentWeather] = useState({})
+class Home extends Component {
 
-        // downloadCurrentWeather = () => {
-        //     fetch()
-        // }
-   
-        return(
-            <div>
+    state = {
+        data: [],
+        isLoaded: false,
+        error: null,
+    }
+
+    FETCH_URL = "http://api.openweathermap.org/data/2.5/weather?q=Gdansk&units=metric&appid=2e2ff6c3d5791be198f04c78b94573e5"
+
+    componentDidMount() {
+        fetch(this.FETCH_URL)
+            .then(response => {console.log('response DATA',response); return (response.json())})
+            .then(result => {
+                console.log('result DATA', result)
+                this.setState({
+                    isLoaded: true,
+                    data: result.data
+                })
+            })
+            .then(error => {
+                this.setState({
+                    data: {},
+                    isLoaded: true,
+                    error
+                });
+            })
+    };
+
+    render() {
+        return (
+            <>
                 <h1>Probably the best weather App in the World</h1>
                 <h3>Na tej stronie będziemy wyświetlać główną stronę aplikacji.</h3>
-            </div>
+            </>
         )
+    }
 }
 
 export default Home;
