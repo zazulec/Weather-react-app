9b57a15f42c8210c6f44de4c2f24c191cdde3d8c
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-03 20:49:56 +0200

    testing display 1st data from api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,55 +5,59 @@ import React, { Component } from 'react';
 class Home extends Component {
 
     state = {
-        data: [],
+        data: this.getInitialDataState(),
         isLoaded: false,
         error: null,
-        cityName: '',
+        currentTemp: {},
     }
 
     FETCH_URL = "http://api.openweathermap.org/data/2.5/weather?q=Gdansk&units=metric&appid=2e2ff6c3d5791be198f04c78b94573e5"
 
     componentDidMount() {
         fetch(this.FETCH_URL)
-            .then(response => { console.log('response DATA', response); return (response.json()) })
+            .then(response => (response.json()))
             .then(result => {
-                console.log('result DATA', result)
                 this.setState({
                     isLoaded: true,
                     data: result,
                 })
-                console.log('state DATA', this.state.data.weather[0].main, 'city name', this.state.data.name)
-                console.log('state DATA', this.state.data)
 
             })
-            .then(error => {
+            .catch(error => {
                 this.setState({
-                    data: [],
+                    data: this.getInitialDataState(),
                     isLoaded: true,
                     error
                 });
             })
     };
 
-
+    getInitialDataState() {
+        return {
+            weather: [],
+            main: {
+                temp: null,
+            },
+        }
+    }
 
     render() {
-        // this.city = this.state.data.map(name => {
-        //     return <div key={id}>{name}</div>
-        // });
-        // const cityName = this.state.data.weather.map(  => {
-        //     return (
-        //         <div key={id}>
-        //             {this.state.data.id}
-        //         </div>
-        //     )
-        // });
+        const { data } = this.state
+        const dayWeather = data.weather.map( element =>
+            <div key={element.id}>
+                {element.description}
+            </div>
+
+        )
+        const currentTemp = data.main.temp;
+        
+
         return (
             <div>
                 <h1>Probably the best weather App in the World</h1>
                 <h3 >Current temp</h3>
-                <p>nie działa</p>
-
+                {dayWeather}
+                {currentTemp}
             </div>
         )
     }
