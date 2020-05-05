1ca538e6b861d9c768664220b5c2d4d24efa225e
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-05 22:20:30 +0200

    display all data on Rechart/next step create filter on each day

--- src/components/WeatherRechart/WeatherRechart.js
+++ src/components/WeatherRechart/WeatherRechart.js
@@ -6,20 +6,20 @@ import { LineChart, Line, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 function WeatherRechart(props) {
 
-    const data = [
+    // const data = [
 
-        { name: props.data.list[0].dt_txt, uv: props.data.list[0].main.temp, pv: 40, amt: 60 },
-        { name: props.data.list[1].dt_txt, uv: props.data.list[1].main.temp, pv: 40, amt: 60 },
-        { name: props.data.list[2].dt_txt, uv: props.data.list[2].main.temp, pv: 40, amt: 60 },
-        // {name: 'Page A', uv: 400, pv: 700, amt: 2400},
-        // {name: 'Page B', uv: 500, pv: 600, amt: 2400},
-        // {name: 'Page C', uv: 600, pv: 500, amt: 2400},
-        // {name: 'Page D', uv: 700, pv: 400, amt: 2400},
-    
-    ];
-    // const data = props.data.list.map(element => [
-    //     { name: element.dt_txt, uv: element.main.temp, pv: 40, amt: 60 },
-    // ]);
+    //     { name: props.data.list[0].dt_txt, uv: props.data.list[0].main.temp, pv: 40, amt: 60 },
+    //     { name: props.data.list[1].dt_txt, uv: props.data.list[1].main.temp, pv: 40, amt: 60 },
+    //     { name: props.data.list[2].dt_txt, uv: props.data.list[2].main.temp, pv: 40, amt: 60 },
+    // {name: 'Page A', uv: 400, pv: 700, amt: 2400},
+    // {name: 'Page B', uv: 500, pv: 600, amt: 2400},
+    // {name: 'Page C', uv: 600, pv: 500, amt: 2400},
+    // {name: 'Page D', uv: 700, pv: 400, amt: 2400},
+
+    // ];
+    const data = props.data.list.map(element => {
+        return { name: element.dt_txt, uv: element.main.temp, pv: 40, amt: 60 }
+    });
 
     return (
         <ResponsiveContainer aspect={1} width="50%" height="50%">
@@ -30,9 +30,9 @@ function WeatherRechart(props) {
                 <XAxis dataKey="name" />
                 <YAxis />
             </LineChart>
-           
+
         </ResponsiveContainer>
-       
+
     );
 }
 
