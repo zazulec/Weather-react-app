8158e73ec58a3a8024f606c4c610795a70b36337
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-10 23:33:40 +0200

    fetch in CurrentWeatherWithGeoLocation works. Next step is display the value of fetch

--- src/components/CurrentWeatherWithGeoLocation/CurrentWeatherWithGeoLocation.js
+++ src/components/CurrentWeatherWithGeoLocation/CurrentWeatherWithGeoLocation.js
@@ -44,11 +44,11 @@ class CurrentWeatherWithGeoLocation extends Component {
             this.FETCH_URL_GEOLOCATION = `http://api.openweathermap.org/data/2.5/weather?lat=${this.state.latitude}&lon=${this.state.longitude}&units=metric&appid=2e2ff6c3d5791be198f04c78b94573e5`
             fetch(this.FETCH_URL_GEOLOCATION)
                 .then(response => (response.json()))
-                .then(result => this.setState({
-                    isLoading: true,
-                    isLoaded: false,   // czy ten then jest ok
-                    data: this.getInitialDataState()
-                }))
+                // .then(result => this.setState({
+                //     isLoading: true,
+                //     // isLoaded: false,   // czy ten then jest ok
+                //     // data: this.getInitialDataState()
+                // }))
                 .then(result => {
                     if (result.cod === 200) {
                         this.setState({
