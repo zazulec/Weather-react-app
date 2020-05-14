d34bee44379f5f3bd0e589e7cd0dbd57bb9c3090
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-14 21:03:41 +0200

    Updates

--- precache-manifest.4119616fe5a3be720b19edccb5bd0813.js
+++ precache-manifest.4119616fe5a3be720b19edccb5bd0813.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd00703b0f3eff3bef078436600e093c",
+    "revision": "0e48ba428ed12ff86bb59bd78126b03a",
     "url": "/Weather-react-app/index.html"
   },
   {
-    "revision": "36ac1df3d0a33b766c84",
+    "revision": "8e7247ddf33fc33c719c",
     "url": "/Weather-react-app/static/css/main.bdeb8c23.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Weather-react-app/static/js/2.1e572946.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "36ac1df3d0a33b766c84",
-    "url": "/Weather-react-app/static/js/main.1fd486c7.chunk.js"
+    "revision": "8e7247ddf33fc33c719c",
+    "url": "/Weather-react-app/static/js/main.6d4b546d.chunk.js"
   },
   {
     "revision": "2de1ed13e5743606910f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Weather-react-app/precache-manifest.848d15781101dc0fad799c390ed5a5a0.js"
+  "/Weather-react-app/precache-manifest.4119616fe5a3be720b19edccb5bd0813.js"
 );
 
 self.addEventListener('message', (event) => {
