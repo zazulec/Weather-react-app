0ecc7b8721eeb72af93d81772c8581a224935681
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-14 21:21:33 +0200

    back App.js to src catalog  probably this thing caused bug on github pages ( display only page 404)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useMemo } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from '../pages/Home/Home';
-import Header from '../components/Header/Header';
-import DefaultPage from '../pages/DefaultPage/DefaultPage';
-import FiveDaysForecast from '../pages/FiveDaysForecast/FiveDaysForecast';
-import SiteNavigation from '../components/SiteNavigation/SiteNavigation';
-import RechartInputContext from '../context/RechartInputContext';
-import CurrentWeatherWithLocation from '../pages/CurrentWeatherWithLocation/CurrentWeatherWithLocation';
-import { StyledNav } from './style/StyledApp';
-import { Overlay } from './style/StyledOverlay';
+import Home from './pages/Home/Home';
+import Header from './components/Header/Header';
+import DefaultPage from './pages/DefaultPage/DefaultPage';
+import FiveDaysForecast from './pages/FiveDaysForecast/FiveDaysForecast';
+import SiteNavigation from './components/SiteNavigation/SiteNavigation';
+import RechartInputContext from './context/RechartInputContext';
+import CurrentWeatherWithLocation from './pages/CurrentWeatherWithLocation/CurrentWeatherWithLocation';
+import { StyledNav } from './StyledApp';
+import { Overlay } from './StyledAppOverlay';
 
 
 function App() {
