17a395bbafd16fce25e35593dcad44b165279cf1
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-15 13:53:46 +0200

    Updates

---
+++ precache-manifest.48bbe1ff38df9ea66d6d03137cbf904f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca26d44af5872868d0e616e9d27e8018",
+    "url": "/Weather-react-app/index.html"
+  },
+  {
+    "revision": "f5b61bad51272c21e493",
+    "url": "/Weather-react-app/static/css/main.bdeb8c23.chunk.css"
+  },
+  {
+    "revision": "9d399082c29e4cdca3cb",
+    "url": "/Weather-react-app/static/js/2.b9aacceb.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/Weather-react-app/static/js/2.b9aacceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b61bad51272c21e493",
+    "url": "/Weather-react-app/static/js/main.a02c47c8.chunk.js"
+  },
+  {
+    "revision": "2de1ed13e5743606910f",
+    "url": "/Weather-react-app/static/js/runtime-main.0dba7e14.js"
+  },
+  {
+    "revision": "a66c554d614d406270426ed589c58b7c",
+    "url": "/Weather-react-app/static/media/man-3915438_1920.a66c554d.jpg"
+  }
+]);
