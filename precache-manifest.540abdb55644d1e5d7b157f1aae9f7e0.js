40a573bab6e97ca1ac7075f549a62024d6c476d5
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-15 20:01:56 +0200

    Updates

---
+++ precache-manifest.540abdb55644d1e5d7b157f1aae9f7e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a477923797ad11e33f6ef6f5c2ae6f99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94cfe0a527daa412f9d3",
+    "url": "/static/css/main.7f986d01.chunk.css"
+  },
+  {
+    "revision": "b055f7c2c9e4d3ebba81",
+    "url": "/static/js/2.27afbe08.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.27afbe08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cfe0a527daa412f9d3",
+    "url": "/static/js/main.9b593079.chunk.js"
+  },
+  {
+    "revision": "5855bf49084dc0464ab6",
+    "url": "/static/js/runtime-main.b71c37de.js"
+  },
+  {
+    "revision": "a66c554d614d406270426ed589c58b7c",
+    "url": "/static/media/man-3915438_1920.a66c554d.jpg"
+  }
+]);
