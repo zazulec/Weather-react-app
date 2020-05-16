82192cf5c6ccd94281a63f5792cc28ce3f45cd6d
zazulec <60104519+zazulec@users.noreply.github.com>
2020-05-16 17:40:16 +0200

    Updates

---
+++ precache-manifest.a526d54433f2df4025ab5731f344dfaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa346adb248f84ee7876abf8ca200ba9",
+    "url": "/Weather-react-app/index.html"
+  },
+  {
+    "revision": "5596d03daf5e14cddd10",
+    "url": "/Weather-react-app/static/css/main.bdeb8c23.chunk.css"
+  },
+  {
+    "revision": "de5b58638d7dadb88f5b",
+    "url": "/Weather-react-app/static/js/2.8227831b.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/Weather-react-app/static/js/2.8227831b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5596d03daf5e14cddd10",
+    "url": "/Weather-react-app/static/js/main.0fbf0d0b.chunk.js"
+  },
+  {
+    "revision": "2de1ed13e5743606910f",
+    "url": "/Weather-react-app/static/js/runtime-main.0dba7e14.js"
+  },
+  {
+    "revision": "a66c554d614d406270426ed589c58b7c",
+    "url": "/Weather-react-app/static/media/man-3915438_1920.a66c554d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcb50aafada3a29fbb2ef064893317d2.js"
+  "/Weather-react-app/precache-manifest.a526d54433f2df4025ab5731f344dfaf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Weather-react-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
