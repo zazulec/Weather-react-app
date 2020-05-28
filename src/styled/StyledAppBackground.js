ef510354fcaf950cef554785654fbf19774bc1e4
Paweł Zarzycki <pawelzarzyckimail@gmail.com>
2020-05-28 13:08:56 +0200

    background in current weather component can change via fetch data

--- src/styled/StyledAppBackground.js
+++ src/styled/StyledAppBackground.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import normal from '../img/background/man-3915438_1920.jpg';
 import clouds from '../img/background/clouds-3353159_1920.jpg';
 import lightning from '../img/background/lightning-1158027_1920.jpg';
-import boy from '../img/background/person-731165_1920.jpg';
+import rain from '../img/background/person-731165_1920.jpg';
 import sky from '../img/background/sky-690293_1920.jpg';
 
 import dawn from '../img/background/dawn-190055_1280.jpg'; // słaba szerokość
@@ -16,28 +16,30 @@ function switchBackground(props) {
     const initialData = {
         name: '',
         weather:[
-            {id: 802, main: "Clouds", description: "scattered clouds", icon: "03d"},
+            {},
+            // {id: 802, main: "Clouds", description: "scattered clouds", icon: "03d"},
         ],
     };
     const backgroundData = state.reduxData || initialData;
-    const setBackground = backgroundData.weather.map(element => element.main);
+    const setBackground = backgroundData.weather.map(element => element.main).toString();
     
     console.log('state', state.reduxData);
     console.log('backgroundData', backgroundData);
     console.log('setBackground', setBackground);
-    
+
     switch (setBackground) {
         case 'Sky':
             return sky;
         case 'Lightning':
             return lightning;
-        case 'Boy':
-            return boy;
+        case 'Rain':
+            return rain;
         case "Clouds":
             return clouds;
         default:
             return normal;
     }
+    
 }
 
 const StyledAppBackground = styled.div`
